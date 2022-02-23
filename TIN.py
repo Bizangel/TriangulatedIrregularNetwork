aa2cefec23757324cedb6099f2ba9d4b9ef5c6b7
Luis De Ávila <luis.deavila@urosario.edu.co>
2022-02-23 18:32:19 -0500

    Update

--- TIN.py
+++ TIN.py
@@ -17,16 +17,23 @@ class TIN:
     def Initialize(self):
         points = np.column_stack((self.x, self.y))
         self.triangulation = Delaunay(points)
-        print(points.shape)
         # Delaunay()
 
     def plotTriangulation(self):
-        pass
+        plt.triplot(self.x,  self.y, self.triangulation.simplices)
+        plt.plot(self.x, self.y, 'o', markersize=2)
+        plt.show()
+
+    def findElevation(self, x, y):
+        index = Delaunay.find_simplex(
+            self.triangulation, [(x, y)], bruteforce=False, tol=None)
+
+        p0, p1, p2 = self.triangulation.simplices[index][0]
+        p0, p1, p2 = self.triangulation.points[p0], self.triangulation.points[p1], self.triangulation.points[p2]
 
     def plotAltitude(self):
         fig = plt.figure()
         ax = plt.axes(projection='3d')
-
         ax.plot_trisurf(self.x, self.y, self.altitude,
                         color='white', cmap="BrBG", alpha=1)
         plt.show()
@@ -34,7 +41,8 @@ class TIN:
 
 x = np.genfromtxt("pts1000c.dat")
 
-print(x[:, 1].shape)
 myterrain = TIN(x)
 
-myterrain.plotAltitude()
+myterrain.findElevation(0, 0)
+# myterrain.plotTriangulation()
+# myterrain.plotAltitude()
