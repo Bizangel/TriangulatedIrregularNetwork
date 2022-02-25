5813f5e20c3d78fde594956edf1a733b07987893
Luis De Avila <luis.deavila@urosario.edu.co>
2022-02-25 14:36:34 -0500

    Added generations

--- TIN.py
+++ TIN.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import Delaunay
 
+from perlin import PerlinNoiseFactory
+
 
 class TIN:
     '''
@@ -39,10 +41,43 @@ class TIN:
         plt.show()
 
 
-x = np.genfromtxt("pts1000c.dat")
+'''Read from input'''
+# x = np.genfromtxt("pts1000c.dat")
 
-myterrain = TIN(x)
+'''Generate Input: '''
+PNFactory = PerlinNoiseFactory(2, octaves=1)
 
-myterrain.findElevation(0, 0)
+'''Evenly sampled generation'''
+# frameSize = 10
+# X, Y = np.meshgrid(np.linspace(-2, 2, frameSize),
+#                    np.linspace(-2, 2, frameSize))
+
+# noise = np.zeros([frameSize, frameSize])
+# for i in range(frameSize):
+#     for j in range(frameSize):
+#         noise[i, j] = PNFactory(i/frameSize, j/frameSize)
+
+
+# elevation = np.reshape(noise, (-1,))
+# datapoints = np.column_stack([X, Y, np.reshape(noise, (-1,))])
+
+'''Non-evenly sampled generation'''
+
+sz_datapoints = 200
+
+X = np.zeros(sz_datapoints)
+Y = np.zeros(sz_datapoints)
+elevation = np.zeros(sz_datapoints)
+for i in range(sz_datapoints):
+    x, y = np.random.random(2)*2 - 1
+    X[i], Y[i] = x, y
+    elevation[i] = PNFactory(x, y)
+
+datapoints = np.column_stack([X, Y, elevation])
+
+myterrain = TIN(datapoints)
+
+
+# myterrain.findElevation(0, 0)
 # myterrain.plotTriangulation()
 # myterrain.plotAltitude()
